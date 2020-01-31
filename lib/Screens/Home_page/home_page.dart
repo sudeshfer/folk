cf8e1a276d9a953e3fcd2ae487f6a020d1504eb5
Sudesh Fernandezz <38062544+sudeshfer@users.noreply.github.com>
2020-01-31 14:13:11 +0530

    home page created

---
+++ lib/Screens/Home_page/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  Homepage({Key key}) : super(key: key);
+
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       body: Container(
+         child: Center(
+           child: Text("Welcome to hjome page"),
+         ),
+       ),
+    );
+  }
+}
