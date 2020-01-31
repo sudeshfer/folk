ff074909aaea616b29bca008449f6c4edc946ede
Sudesh Fernandezz <38062544+sudeshfer@users.noreply.github.com>
2020-01-31 17:12:14 +0530

    fblogin endpoint intergrated

---
+++ lib/Controllers/ApiServices/FbLoginService.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'package:http/http.dart' as http;
+import 'package:folk/Controllers/ApiServices/variables.dart';
+
+class FbLoginService {
+  static Future<bool> FbAuth(body) async {
+    final response = await http.post('${URLS.BASE_URL}/user/loginwithfb', body: body);
+
+    final data = response.body;
+
+    Map<String, dynamic> user = jsonDecode(data);
+    log(user['loginstatus']);
+
+    if (user['loginstatus'] == 'newuser') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
