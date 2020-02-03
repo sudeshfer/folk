de92bbbf633d66cecac8c048d96a6380a81a58cb
Sudesh Fernandezz <38062544+sudeshfer@users.noreply.github.com>
2020-02-03 10:48:28 +0530

    few modification

--- lib/Controllers/ApiServices/FbLoginService.dart
+++ lib/Controllers/ApiServices/FbLoginService.dart
@@ -4,13 +4,16 @@ import 'package:http/http.dart' as http;
 import 'package:folk/Controllers/ApiServices/variables.dart';
 
 class FbLoginService {
-  static Future<bool> FbAuth(body) async {
+  static Future<bool> fbAuth(body) async {
     final response = await http.post('${URLS.BASE_URL}/user/loginwithfb', body: body);
 
     final data = response.body;
 
     Map<String, dynamic> user = jsonDecode(data);
     log(user['loginstatus']);
+    // print(user['loginstatus']);
+
+
 
     if (user['loginstatus'] == 'newuser') {
       return true;
